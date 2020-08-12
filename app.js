d0cc772c9f51eebe6ea48b221c77d250e36fee56
linus muema <linus.m.muema@gmail.com>
2020-08-12 12:39:47 +0300

    - added the email endpoints

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
 const express = require('express')
 const app = express()
+const nodemailer = require('nodemailer')
+const pug = require('pug')
 
 app.use(express.json());
 app.listen('2400', () => {console.log('server started on port 2400')})
+
+//Nodemailer config
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'skyla.hamill12@ethereal.email',
+        pass: 'xCRr3ARsnMwwUrYa5U'
+    }
+});
+
+app.get('/email/text', (req, res) => {
+    transporter.sendMail({
+        from: '"moose" <me@moose.dev>',
+        to: '"You there" <you@there.com>',
+        subject: 'First email',
+        html: 'Hello, nice to meet you'
+    })
+        .then(info => res.send(info))
+        .catch(error => res.send(error))
+})
+
+app.get('/email/html', (req, res) => {
+        transporter.sendMail({
+            from: '"moose" <me@moose.dev>',
+            to: '"You there" <you@there.com>',
+            subject: 'Second email',
+            html: '<h1>Hi again</h1> <p>This time we have used html!</p>'
+        })
+            .then(info => res.send(info))
+            .catch(error => res.send(error))
+})
+
+app.get('/email/file', (req, res) => {
+        let body = pug.renderFile('views/email.pug')
+        transporter.sendMail({
+            from: '"moose" <me@moose.dev>',
+            to: '"You there" <you@there.com>',
+            subject: 'Third email',
+            html: body
+        })
+            .then(info => res.send(info))
+            .catch(error => res.send(error))
+})
+
 
 module.exports = app;
